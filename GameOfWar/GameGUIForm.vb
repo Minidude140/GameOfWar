53dc3164c2ec3018f58949119cf983c9ff1abc99
Zac <zac.minidude140@Gmail.com>
2024-09-22 21:49:27 -0600

    Created Initial Headers

--- GameOfWar/GameGUIForm.vb
+++ GameOfWar/GameGUIForm.vb
@@ -1,3 +1,11 @@
-﻿Public Class GameGUIForm
+﻿'Zachary Christensen
+'Fall 2024; 09/22/24
+'Card Game of War GUI Form
+'https://github.com/Minidude140/GameOfWar
+
+'This Form Handles the GUI Input and Output to and from the User
+
+
+Public Class GameGUIForm
 
 End Class
--- GameOfWar/GameMechanicsModule.vb
+++ GameOfWar/GameMechanicsModule.vb
@@ -1,3 +1,11 @@
-﻿Module GameMechanicsModule
+﻿'Zachary Christensen
+'Fall 2024; 09/22/24
+'Card Game of War Mechanics Module
+'https://github.com/Minidude140/GameOfWar
+
+'This Module Will Handle the Game Mechanics including, Shuffling the Deck, Drawing Cards, and Comparing Hands
+
+
+Module GameMechanicsModule
 
 End Module
