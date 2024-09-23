e4029966abcdb2e367edf2106f472e617fbb004a
Zac <zac.minidude140@Gmail.com>
2024-09-22 22:19:37 -0600

    Created Card Deck Array

--- GameOfWar/GameMechanicsModule.vb
+++ GameOfWar/GameMechanicsModule.vb
@@ -6,7 +6,7 @@
 'This Module Will Handle the Game Mechanics including, Shuffling the Deck, Drawing Cards, and Comparing Hands
 
 'TODO
-'[]Keep Track of Card Value and Drawn Status
+'[*]Keep Track of Card Value and Drawn Status (Array Location Tells Value)
 '[]Create Draw Card Sub
 '[]Give Drawn Card to Chosen Player
 '[]Compare Cards
@@ -15,5 +15,21 @@
 
 
 Module GameMechanicsModule
+    Dim cardDeck(12, 3) As Boolean
+    '       HRT     DIA     CLB     SPD
+    'A      0,0     0,1     0,2     0,3
+    '2      1,0
+    '3      2,0
+    '4      3,0
+    '5      4,0
+    '6      5,0
+    '7      6,0
+    '8      7,0
+    '9      8,0
+    '10     9,0
+    'J      10,0
+    'Q      11,0
+    'K      12,0
+
 
 End Module
